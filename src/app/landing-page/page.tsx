ef6398f09c18fcc6084eff7e3a87a3a36d7b022f
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-10-04 11:57:58 -0600

    Docs section added

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -5,7 +5,16 @@ import TerminalComponent from "@/components/Terminal";
 import Iphone15Pro from "@/components/Iphone15pro";
 import { MarqueeDemo } from "@/components/Marquee";
 import { Item } from "@/types/interfaces";
-import { FolderSync, LayoutTemplate, PaintbrushVertical } from "lucide-react";
+import {
+  ArrowUp01,
+  BookOpen,
+  FolderSync,
+  LayoutTemplate,
+  PaintbrushVertical,
+  Terminal,
+  TerminalIcon,
+} from "lucide-react";
+import Link from "next/link";
 
 const notifications: Item[] = [
   {
@@ -33,7 +42,6 @@ const notifications: Item[] = [
     color: "#FFB800",
   },
 ];
-
 
 export default function LandingPage() {
   return (
@@ -102,6 +110,14 @@ export default function LandingPage() {
                   invitados y confirmaciones.
                 </p>
               </li>
+              <li className="flex items-center w-full gap-6">
+                <Terminal size={36} color="#4461F2" />
+                <p>
+                  {" "}
+                  Tú solución a unos clics gracias a nuestra tecnología{" "}
+                  <strong className="text-dark-blue">No Code</strong>.
+                </p>
+              </li>
             </ul>
           </div>
         </article>
@@ -109,13 +125,75 @@ export default function LandingPage() {
 
       <section className="flex flex-col gap-8 items-center mt-[10rem]">
         <h2 className="text-3xl text-center font-medium text-strong-blue">
-          Dale un vistazo a los eventos actuales que están en <strong className="text-light-blue">BuildMyEvent</strong>
+          Dale un vistazo a los eventos actuales que están en{" "}
+          <strong className="text-light-blue">BuildMyEvent</strong>
         </h2>
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg">
           <MarqueeDemo />
           <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
           <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
         </div>
+      </section>
+
+      <section className="mt-[5rem] flex gap-16 flex-col">
+        <h2 className="text-3xl text-center font-medium text-strong-blue">
+          Si eres desarrollador dale un vistazo a nuestra{" "}
+          <strong className="text-light-blue">Documentación</strong>
+        </h2>
+        <article className="flex w-full justify-evenly">
+          <div className="w-[40%]">
+            <ul className="text-xl font-raleway h-full flex flex-col gap-6 justify-center">
+              <li className="flex items-center w-full gap-6">
+                <PaintbrushVertical size={36} color="#4461F2" />
+                <p>
+                  <strong className="text-dark-blue">Crea</strong> tu evento
+                  utilizando nuestra Api.
+                </p>
+              </li>
+              <li className="flex items-center w-full gap-6">
+                <BookOpen size={36} color="#4461F2" />
+                <p>
+                  <strong className="text-dark-blue">Implementa</strong>{" "}
+                  fácilmente en tu página.
+                </p>
+              </li>
+              <li className="flex items-center w-full gap-6">
+                <ArrowUp01 size={36} color="#4461F2" />
+                <p>
+                  {" "}
+                  Obtén <strong className="text-dark-blue">métricas</strong>
+                  importantes de tus eventos.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="w-[46%]">
+            <TerminalComponent>
+              <div className="flex items-center mb-4">
+                <TerminalIcon className="text-blue-600 mr-2" />
+                <h3 className="text-lg font-semibold">Terminal</h3>
+              </div>
+              <div className="bg-gray-50 p-4 rounded text-sm font-mono leading-relaxed border border-gray-300">
+                <code>
+                  <span className="text-blue-700">curl</span> -X GET \
+                  <br />
+                  <span className="text-indigo-500">
+                    https://api.ejemplo.com/v1/recurso
+                  </span>{" "}
+                  \
+                  <br />
+                  <span className="text-red-500">-H</span> "Authorization:
+                  Bearer YOUR_ACCESS_TOKEN"
+                </code>
+              </div>
+              <div className="w-full flex justify-center">
+                <Link href="/docs">
+                  <ShimmerButton>Visita la documentación</ShimmerButton>
+                </Link>
+              </div>
+            </TerminalComponent>
+          </div>
+        </article>
       </section>
     </>
   );

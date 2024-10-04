6c0e8cb16b8c1ba32145fe80a11bc65b80e7aeb8
KevinLatino <latinokevin9@gmail.com>
2024-10-04 01:01:42 -0600

    Structural changes on Landing-Page

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -69,11 +69,11 @@ export default function LandingPage() {
         </div>
       </section>
 
-      <section className="mt-[20rem] flex gap-16 flex-col">
+      <section className="mt-[10rem] flex gap-16 flex-col">
         <h2 className="text-3xl text-center font-medium text-strong-blue">
-          Con <strong className="text-light-blue">BuildMyEvent</strong> puedes
+        ¡Con <strong className="text-light-blue">BuildMyEvent</strong> puedes
           crear tus diferentes{" "}
-          <strong className="text-light-blue">eventos</strong> !
+          <strong className="text-light-blue">eventos</strong>!
         </h2>
         <article className="flex w-full justify-evenly">
           <div className="w-[46%]">
@@ -111,10 +111,9 @@ export default function LandingPage() {
         </article>
       </section>
 
-      <section className="flex flex-col gap-8 items-center mt-32">
-        <h2 className="text-black text-4xl">
-          Clientes que confiaron en{" "}
-          <strong className="text-light-blue">BuildMyEvent.</strong>
+      <section className="flex flex-col gap-8 items-center mt-[10rem]">
+        <h2 className="text-3xl text-center font-medium text-strong-blue">
+          Dale un vistazo a los eventos actuales que están en <strong className="text-light-blue">BuildMyEvent</strong> 
         </h2>
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg">
           <MarqueeDemo />

11921c3d14d397dd04db31f743620268bf2e503b
KevinLatino <latinokevin9@gmail.com>
2024-10-04 01:16:04 -0600

    deleting time from iphone

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -11,28 +11,24 @@ const notifications: Item[] = [
   {
     name: "Crea un nuevo evento",
     description: "Crea tu evento en cuestión de minutos",
-    time: "",
     icon: "🚀",
     color: "#1E86FF",
   },
   {
     name: "Personaliza tu evento",
     description: "Tienes plantillas a tu disposición",
-    time: "5m ago",
     icon: "🎨",
     color: "#FF3D71",
   },
   {
     name: "Recibe pagos",
     description: "Recibe pagos de tus clientes",
-    time: "15m ago",
     icon: "💸",
     color: "#00C9A7",
   },
   {
     name: "Usa nuestra API",
     description: "Puedes usar nuestra API",
-    time: "10m ago",
     icon: "👨🏻‍💻",
     color: "#FFB800",
   },
@@ -125,7 +121,7 @@ export default function LandingPage() {
   );
 }
 
-const Notification = ({ name, description, icon, color, time }: Item) => {
+const Notification = ({ name, description, icon, colors }: Item) => {
   return (
     <figure className="relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-2xl p-4 transition-all duration-200 ease-in-out hover:scale-[103%] bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
       <div className="flex flex-row items-center gap-3">
@@ -141,7 +137,7 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
           <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white">
             <span className="text-sm sm:text-lg">{name}</span>
             <span className="mx-1">·</span>
-            <span className="text-xs text-gray-500">{time}</span>
+            <span className="text-xs text-gray-500"></span>
           </figcaption>
           <p className="text-sm font-normal dark:text-white/60">
             {description}

9a3b4bfbfe6d41420ad19f71b81d8f8f8daebeb3
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-10-06 20:31:32 -0600

    Navbar updated

--- src/app/console/navbar.tsx
+++ src/app/console/navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
+import BME from "../../../public/BME-Logos/BME-Logo-Over-White1.svg";
+import Image from "next/image";
+import { LogOut } from "lucide-react";
 
 export default function Header() {
   const [selectedTab, setSelectedTab] = useState<string | null>(null);
@@ -12,7 +12,7 @@ export default function Header() {
   const router = useRouter();
 
   const navOptions = [
-    { label: "Home", route: "/" },
+    { label: "Home", route: "" },
     { label: "My Page", route: "/my-page" },
     { label: "Events", route: "/events" },
     { label: "Orders", route: "/orders" },
@@ -20,22 +20,24 @@ export default function Header() {
 
   useEffect(() => {
     const adjustedPathName = pathname.split("/")[2] || "";
-    console.log(adjustedPathName)
+    console.log(adjustedPathName);
     setSelectedTab(adjustedPathName);
   }, [pathname]);
 
-  console.log(pathname)
+  console.log(pathname);
 
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto p-6 flex items-center justify-between">
-        <div className="text-3xl font-bold text-strong-blue">BuildMyEvent</div>
+        <div className="text-3xl font-bold text-strong-blue">
+          <Image src={BME} alt="Buildmyevent" width={200} />
+        </div>
         <Button
           variant="outline"
-        //   onClick={() => ()}
           className="border-light-blue text-light-blue transition-opacity opacity-80 hover:opacity-100"
         >
-          Sign Out
+          <LogOut/>
+          Log Out
         </Button>
       </div>
 
@@ -43,7 +45,7 @@ export default function Header() {
         {navOptions.map((option) => (
           <button
             key={option.route}
-            onClick={() => router.push(option.route)}
+            onClick={() => router.push("/console" + option.route)}
             className={`text-lg px-4 py-2 rounded-md transition-opacity duration-200 ${
               selectedTab === option.route.slice(1)
                 ? "text-light-blue border-b-2 border-light-blue opacity-100"
